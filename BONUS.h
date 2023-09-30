7f8ea2b6130070bbbd471d876e8f682561dc92eb
Bebra <trishechkin.sasha@yandex.ru>
2023-09-30 13:44:02 +0700

    cosmetic changes

--- BONUS.h
+++ BONUS.h
@@ -44,24 +44,24 @@ struct sale {
 
 void create_shop(shop* shop_cr) {
 	char shop_name[SHOP_NAME_SIZE];
-	printf("Введите название магазина");
+	printf("Введите название магазина ");
 	gets_s(shop_name, SHOP_NAME_SIZE);
 	*shop_cr->name = *shop_name;
 	shop_cr->items_counter = 0;
-	printf("Создан магазин %s ", shop_name);
+	printf("Создан магазин %s\n", shop_name);
 }
 
 void shop_add_item(shop* shop_add_item) {
 	char item_name[ITEM_NAME_SIZE];
 	int item_price, item_discount;
-	printf("Введите название товара");
+	printf("Введите название товара ");
 	gets_s(item_name, ITEM_NAME_SIZE);
 	*shop_add_item->items[shop_add_item->items_counter].name = *item_name;
-	printf("Введите цену товара");
+	printf("Введите цену товара ");
 	scanf("%d", &item_price);
 	shop_add_item->items[shop_add_item->items_counter].price = item_price;
-	printf("Введите скидку");
+	printf("Введите скидку ");
 	scanf("%d", &item_discount);
 	shop_add_item->items[shop_add_item->items_counter].discount = item_discount;
-	printf("Добавлен товар %s по цене %d со скидкой %d/%/", item_name, item_price, item_discount);
+	printf("\nДобавлен товар %s по цене %d со скидкой %d", item_name, item_price, item_discount);
 }
--- Zadanie.cpp
+++ Zadanie.cpp
@@ -8,5 +8,6 @@ int main()
     setlocale(LC_ALL, "Rus");
     shop shops[SHOP_COUNT];
     create_shop(&(shops[0]));
+    shop_add_item(&(shops[0]));
 }
 
