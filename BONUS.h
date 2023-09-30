c4fc2cf7775c15cff579e2288175ce009d283036
Bebra <trishechkin.sasha@yandex.ru>
2023-09-30 14:15:57 +0700

    added item delete

--- BONUS.h
+++ BONUS.h
@@ -7,39 +7,39 @@
 #define SALE_TEXT_LENGTH 100
 
 struct item {
-	char name[ITEM_NAME_SIZE];
-	int price;
-	int discount;
+	char name[ITEM_NAME_SIZE] = "0";
+	int price = 0;
+	int discount = 0;
 };
 
 struct shop {
-	char name[SHOP_NAME_SIZE];
+	char name[SHOP_NAME_SIZE] = "0";
 	item items[SHOP_ITEMS_COUNT];
-	int items_counter;
+	int items_counter = 0;
 };
 
 struct expire {
 	struct date {
-		int day;
-		int month;
-		int year;
+		int day = 0;
+		int month = 0;
+		int year = 0;
 	};
 	struct time {
-		int second;
-		int minute;
-		int hour;
+		int second = 0;
+		int minute = 0;
+		int hour = 0;
 	};
 };
 
 struct promocode {
 	item item;
-	char code[CODE_SIZE];
+	char code[CODE_SIZE] = "0";
 	expire expire;
 };
 
 struct sale {
 	item items[SALE_ITEMS_COUNT];
-	char text[SALE_TEXT_LENGTH];
+	char text[SALE_TEXT_LENGTH] = "0";
 };
 
 void create_shop(shop* shop_cr) {
@@ -47,7 +47,6 @@ void create_shop(shop* shop_cr) {
 	printf("Введите название магазина ");
 	gets_s(shop_name, SHOP_NAME_SIZE);
 	*shop_cr->name = *shop_name;
-	shop_cr->items_counter = 0;
 	printf("Создан магазин %s\n", shop_name);
 }
 
@@ -71,3 +70,31 @@ void item_info(item* current_item) {
 	printf("%s по цене %d со скидкой %d", current_item->name, current_item->price, current_item->discount);
 }
 
+void change_item_name(item* current_item){
+	char item_name[ITEM_NAME_SIZE];
+	printf("Введите название товара ");
+	gets_s(item_name, ITEM_NAME_SIZE);
+	*current_item->name = *item_name;
+}
+
+void change_item_price(item* current_item) {
+	int item_price;
+	printf("Введите цену товара ");
+	scanf("%d", &item_price);
+	current_item->price = item_price;
+}
+
+void change_item_discount(item* current_item) {
+	int item_discount;
+	printf("Введите скидку на товар ");
+	scanf("%d", &item_discount);
+	current_item->price = item_discount;
+}
+
+void delete_item(shop* shop_delete_item, int item_to_delete) {
+	item buffer;
+	*shop_delete_item->items[item_to_delete].name = *("0");
+	shop_delete_item->items[item_to_delete].price = 0;
+	shop_delete_item->items[item_to_delete].discount = 0;
+	shop_delete_item->items_counter--;
+}
