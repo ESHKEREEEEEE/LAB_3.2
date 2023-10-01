90c2fe382cebeecea5ec86db0acc4cdcb388e2b7
Bebra <trishechkin.sasha@yandex.ru>
2023-10-01 14:23:57 +0700

    added kapec kak mnogo vsego

--- BONUS.h
+++ BONUS.h
@@ -5,6 +5,7 @@
 #define SHOP_ITEMS_COUNT 30
 #define SALE_ITEMS_COUNT 30
 #define SALE_TEXT_LENGTH 100
+#define PROMOCODE_ITEM_SIZE 30
 
 struct item {
 	char name[ITEM_NAME_SIZE] = "0";
@@ -18,23 +19,30 @@ struct shop {
 	int items_counter = 0;
 };
 
-struct expire {
-	struct date {
-		int day = 0;
-		int month = 0;
-		int year = 0;
-	};
-	struct time {
-		int second = 0;
-		int minute = 0;
-		int hour = 0;
-	};
+
+struct date {
+	int day = 0;
+	int month = 0;
+	int year = 0;
 };
 
+struct timer {
+	int second = 0;
+	int minute = 0;
+	int hour = 0;
+};
+
+struct expire {
+	date date;
+	timer time;
+};
+
+
 struct promocode {
-	item item;
+	item items[PROMOCODE_ITEM_SIZE];
 	char code[CODE_SIZE] = "0";
 	expire expire;
+	int items_counter = 0;
 };
 
 struct sale {
@@ -50,20 +58,36 @@ void create_shop(shop* shop_cr) {
 	printf("Создан магазин %s\n", shop_name);
 }
 
-void shop_add_item(shop* shop_add_item) {
+void add_item(shop* add_item) {
 	char item_name[ITEM_NAME_SIZE];
 	int item_price, item_discount;
 	printf("Введите название товара ");
 	gets_s(item_name, ITEM_NAME_SIZE);
-	*shop_add_item->items[shop_add_item->items_counter].name = *item_name;
+	*add_item->items[add_item->items_counter].name = *item_name;
 	printf("Введите цену товара ");
 	scanf("%d", &item_price);
-	shop_add_item->items[shop_add_item->items_counter].price = item_price;
+	add_item->items[add_item->items_counter].price = item_price;
 	printf("Введите скидку ");
 	scanf("%d", &item_discount);
-	shop_add_item->items[shop_add_item->items_counter].discount = item_discount;
+	add_item->items[add_item->items_counter].discount = item_discount;
 	printf("\nДобавлен товар %s по цене %d со скидкой %d", item_name, item_price, item_discount);
-	shop_add_item->items_counter++;
+	add_item->items_counter++;
+}
+
+void add_item(promocode* add_item) {
+	char item_name[ITEM_NAME_SIZE];
+	int item_price, item_discount;
+	printf("Введите название товара ");
+	gets_s(item_name, ITEM_NAME_SIZE);
+	*add_item->items[add_item->items_counter].name = *item_name;
+	printf("Введите цену товара ");
+	scanf("%d", &item_price);
+	add_item->items[add_item->items_counter].price = item_price;
+	printf("Введите скидку ");
+	scanf("%d", &item_discount);
+	add_item->items[add_item->items_counter].discount = item_discount;
+	printf("\nДобавлен товар %s по цене %d со скидкой %d", item_name, item_price, item_discount);
+	add_item->items_counter++;
 }
 
 void item_info(item* current_item) {
@@ -97,4 +121,59 @@ void delete_item(shop* shop_delete_item, int item_to_delete) {
 	shop_delete_item->items[item_to_delete].price = 0;
 	shop_delete_item->items[item_to_delete].discount = 0;
 	shop_delete_item->items_counter--;
+}
+
+void change_date_day(date* date_to_change) {
+	int new_day;
+	printf("Введите день");
+	scanf("%d", &new_day);
+	date_to_change->day = new_day;
+}
+
+void change_date_month(date* date_to_change) {
+	int new_month;
+	printf("Введите месяц");
+	scanf("%d", &new_month);
+	date_to_change->month = new_month;
+}
+
+void change_date_year(date* date_to_change) {
+	int new_year;
+	printf("Введите год");
+	scanf("%d", &new_year);
+	date_to_change->month = new_year;
+}
+
+void change_time_hour(timer* time_to_change) {
+	int new_hour;
+	printf("Введите час");
+	scanf("%d", &new_hour);
+	time_to_change->hour = new_hour;
+}
+
+void change_time_minute(timer* time_to_change) {
+	int new_minute;
+	printf("Введите минуту");
+	scanf("%d", &new_minute);
+	time_to_change->minute = new_minute;
+}
+
+void change_time_second(timer* time_to_change) {
+	int new_second;
+	printf("Введите секунду");
+	scanf("%d", &new_second);
+	time_to_change->second = new_second;
+}
+
+void create_promocode(promocode* promocode_cr) {
+	char promocode[CODE_SIZE];
+	printf("Введите промокод ");
+	gets_s(promocode, CODE_SIZE);
+	*promocode_cr->code = *promocode;
+	change_date_day(&(promocode_cr->expire.date));
+	change_date_month(&(promocode_cr->expire.date));
+	change_date_year(&(promocode_cr->expire.date));
+	change_time_second(&(promocode_cr->expire.time));
+	change_time_minute(&(promocode_cr->expire.time));
+	change_time_hour(&(promocode_cr->expire.time));
 }
